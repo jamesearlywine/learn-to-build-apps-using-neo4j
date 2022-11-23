02d4113a57dfa061246dc1f32dd944237f162589
James Earlywine <james@esilogix.com>
2022-11-23 10:08:47 -0500

    ..

--- project/app.stop.js
+++ project/app.stop.js
@@ -1,4 +1,4 @@
-// shamelessly copied from here: https://stackoverflow.com/questions/23258421/how-to-stop-app-that-node-js-express-npm-start
+// https://stackoverflow.com/questions/23258421/how-to-stop-app-that-node-js-express-npm-start
 const io = require('socket.io-client');
 const socketClient = io.connect('http://localhost:3000'); // Specify port if your express server is not using default port 80
 
