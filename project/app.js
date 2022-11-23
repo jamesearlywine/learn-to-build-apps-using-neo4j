a9c6c4dd49530d95c49a401ba97ec0924b0f47a9
James Earlywine <james@esilogix.com>
2022-11-23 10:05:57 -0500

    ..

--- project/app.js
+++ project/app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const routes = require('./src/routes.js');
+const http = require('http');
 
 const app = express();
 
@@ -19,8 +20,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 routes.forApp(app);
 
 const port = 3000;
-app.listen(port);
+server = http.createServer(app);
+server.listen(port);
 
-console.log(`Server started on port ${port}`);
+console.log(`\nServer started on port ${port}\n`);
+
+// https://stackoverflow.com/questions/23258421/how-to-stop-app-that-node-js-express-npm-start
+const io = require('socket.io')(server);
+io.on('connection', (socketServer) => {
+  socketServer.on('npmStop', () => {
+    process.exit(0);
+  });
+});
 
 module.exports = app;
---
+++ project/app.stop.js
@@ -0,0 +1,10 @@
+// shamelessly copied from here: https://stackoverflow.com/questions/23258421/how-to-stop-app-that-node-js-express-npm-start
+const io = require('socket.io-client');
+const socketClient = io.connect('http://localhost:3000'); // Specify port if your express server is not using default port 80
+
+socketClient.on('connect', () => {
+  socketClient.emit('npmStop');
+  setTimeout(() => {
+    process.exit(0);
+  }, 100);
+});
